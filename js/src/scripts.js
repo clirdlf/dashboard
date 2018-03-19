e8dcb70127b17602018ea27ecf0b8d5e7d52e122
Wayne Graham <wgraham@clir.org>
2018-03-19 15:11:10 -0400

    add enforce ssl

---
+++ js/src/scripts.js
@@ -0,0 +1,44 @@
+//= require ../../node_modules/jquery.nicescroll/dist/jquery.nicescroll.js
+
+$(function() {
+    'use strict';
+
+    function scrollToTop() {
+        // verticalOffset = typeof(verticalOffset) !== 'undefined' ? verticalOffset : 0;
+        var element = $('body');
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        $('html, body').animate({
+            scrollTop: offsetTop
+        }, 500, 'linear');
+    }
+
+    $(document).on('scroll', function() {
+
+        if ($(window).scrollTop() > 100) {
+            $('.scroll-top-wrapper').addClass('show');
+        } else {
+            $('.scroll-top-wrapper').removeClass('show');
+        }
+    });
+
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+
+    /*------- Smooth Scroll -------*/
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+
+    });
+
+
+
+
+});
