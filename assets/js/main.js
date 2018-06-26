24f0487f8fef32ec58f40bd76f76cc2ba187da35
Wayne Graham <wgraham@clir.org>
2018-06-26 17:09:16 -0400

    Feature filter
    Adds a  filter to only display items tagged with featured by default, but allows access to all projects
    
    closes #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,31 +30,5 @@ $(document).ready(function() {
     });
 
 
-    /* ==============================================
-     portfolio-filter
-     =============================================== */
-
-    // filter items on button click
-
-    var $grid = $('.grid').isotope({
-        // set itemSelector so .grid-sizer is not used in layout
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-            // use element for option
-            columnWidth: '.grid-sizer'
-        }
-    });
-
-    $grid.imagesLoaded().progress( function() {
-        $grid.isotope('layout');
-    });
-    $('#filtr-container').on( 'click', 'li', function(e) {
-        e.preventDefault();
-        $('#filtr-container li').removeClass('active');
-        $(this).closest('li').addClass('active');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
 
 });
---
+++ js/landing_page_filter.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  'use strict';
+
+  var $grid = $('.grid').isotope({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    filter: '.featured',
+    percentPosition: true,
+    masonry: {
+      // use element for option
+      columnWidth: '.grid-sizer'
+    }
+  });
+
+  $grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
+  
+  $('#filtr-container').on( 'click', 'li', function(e) {
+    e.preventDefault();
+    $('#filtr-container li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+});
