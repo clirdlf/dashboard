d80557df262b35e60a65f4e2dafffdc7f5c8b3f9
Wayne Graham <wgraham@clir.org>
2018-06-29 09:35:16 -0400

    Add map to contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,252 @@
 /*
- * Cocoon -  Portfolio html  Template
- * Build Date: december 2017
- * Author: colorlib
- * Copyright (C) 2018 colorlib
- */
+* Cocoon -  Portfolio html  Template
+* Build Date: december 2017
+* Author: colorlib
+* Copyright (C) 2018 colorlib
+*/
 /* -------------------------------------  */
 /*  TABLE OF CONTENTS
 /* -------------------------------------  */
 /*   PRE LOADING                          */
 /*   SIDEBAR Menu                         */
 /*   Portfolio Masonry                    */
-/*   portfolio-filter                     */
+/*   Maps                                 */
 
 /* ==============================================
 /*  PRE LOADING
-  =============================================== */
+=============================================== */
 $(window).load(function() {
-    $('.loader').delay(500).fadeOut('slow');
+  $('.loader').delay(500).fadeOut('slow');
 });
 
 $(document).ready(function() {
 
-    'use strict';
+  'use strict';
 
-    /* ==============================================
-      Sidebar show and hide
-       ============================================== */
-    $(".menu-btn").on('click',function(i){
-        $("body").toggleClass("sidebar_closed");
+  /* ==============================================
+  Sidebar show and hide
+  ============================================== */
+  $(".menu-btn").on('click',function(i){
+    $("body").toggleClass("sidebar_closed");
+  });
+
+  /* --------------------------------------------------------
+  MAPS
+  ----------------------------------------------------------- */
+  var map = $('#map');
+  if(map.length > 0) {
+    google.maps.event.addDomListener(window, 'load', init);
+    var lattuide = map.attr('data-lat');
+    var longtuided = map.attr('data-lon');
+  }
+  function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+      // How zoomed in you want the map to start at (always required)
+      zoom: 16,
+      scrollwheel: false,
+      navigationControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      // The latitude and longitude to center the map (always required)
+      center: new google.maps.LatLng(lattuide, longtuided), // New York
+
+      // How you would like to style the map.
+      // This is where you would paste any style found on Snazzy Maps.
+      styles: [
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#a3ccff"
+            },
+            {
+              "lightness": 17
+            }
+          ]
+        },
+        {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfdfdf"
+            },
+            {
+              "lightness": 20
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            },
+            {
+              "lightness": 17
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            },
+            {
+              "lightness": 29
+            },
+            {
+              "weight": 0.2
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            },
+            {
+              "lightness": 18
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            },
+            {
+              "lightness": 16
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            },
+            {
+              "lightness": 21
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f9ecd4"
+            },
+            {
+              "lightness": 21
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "visibility": "on"
+            },
+            {
+              "color": "#ffffff"
+            },
+            {
+              "lightness": 16
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "saturation": 36
+            },
+            {
+              "color": "#333333"
+            },
+            {
+              "lightness": 40
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f2f2f2"
+            },
+            {
+              "lightness": 19
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#fefefe"
+            },
+            {
+              "lightness": 20
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#fefefe"
+            },
+            {
+              "lightness": 17
+            },
+            {
+              "weight": 1.2
+            }
+          ]
+        }
+      ]
+    };
+
+    // Get the HTML DOM element that will contain your map
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lattuide, longtuided),
+      map: map,
+      // icon: '/assets/img/map-icon.png',
+      title: 'DLF!'
     });
-
-    /* ==============================================
-     portfolio-filter
-       ============================================== */
-
-    // filter items on button click
+  }
 
 
-    
 });
